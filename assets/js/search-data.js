c772b608aab21166a02e1ee565fa96ba5b2c8fae
otroshi <72525669+otroshi@users.noreply.github.com>
2026-09-02 09:58:31 +0000

    Deploying to gh-pages from @ foundgen-bio/eccv2026@96636efbc21f8410b5108489969c5fc812635c50 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/eccv2026/call_for_paper/";
           },
+        },{id: "nav-program",
+          title: "Program",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/eccv2026/program/";
+          },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
@@ -49,6 +56,17 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "news-submission-deadline-is-extended-to-july-15",
           title: 'Submission deadline is extended to July 15.',
+          description: "",
+          section: "News",},{id: "news-decision-notifications-are-sent-to-authors-congratulations-to-authors-of-accepted-papers",
+          title: 'Decision notifications are sent to authors. Congratulations to authors of accepted papers!',
+          description: "",
+          section: "News",},{id: "news-list-of-accepted-papers-are-available",
+          title: 'List of accepted papers are available.',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/eccv2026/news/announcement_6/";
+            },},{id: "news-workshop-program-is-out",
+          title: 'Workshop program is out!',
           description: "",
           section: "News",},{
       id: 'light-theme',
